5fd5125eec45008caebde6b9c910afb923456ba8
vision <1334822971@qq.com>
2019-07-16 11:09:49 +0800

    feat(global):增加css,图片，字体，xml数据loader

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
+import './style.css'
+import ICON from './icon.png'
+import DATA from './data.xml'
+
 function component() {
   const element = document.createElement("div");
-  element.innerHTML = `hello webpack`;
+  element.innerHTML = `hello webpack !`;
+  element.classList.add('hello');
+  const image = new Image()
+  image.src = ICON
+  element.appendChild(image)
+  console.log(DATA);
   return element;
 }
 document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,37 @@ module.exports = {
   output: {
     filename: "boundle.js",
     path: path.resolve(__dirname, "dist")
+  },
+  module: {
+    rules: [{
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      // {
+      //   test: /\.(png|gif|png|webp|svg)$/,
+      //   use: ['file-loader']
+      // },
+      {
+        test: /\.(png|gif|png|webp|svg)$/,
+        use: [{
+          loader: 'url-loader',
+          options: {
+            limit: 4000
+          }
+        }]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.(csv|tsv)$/,
+        use: ['csv-loader']
+      },
+      {
+        test: /\.xml$/,
+        use: ['xml-loader']
+      }
+    ]
   }
 };
